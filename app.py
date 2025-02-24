81dba3c9fcdce9946fff7c85d2de45ca3fbb0417
Gautam Kumar <gautam.kumar@hva.nl>
2025-02-24 10:31:34 +0100

    Changing the personal route and displaying the data from db

--- app.py
+++ app.py
@@ -2,7 +2,29 @@ import csv
 from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
+# mysqlconnector
+import pandas as pd
+from sqlalchemy import create_engine, text
+import mysql.connector
 
+host = 'oege.ie.hva.nl'
+database = 'zkumarg'
+user = 'kumarg'
+password = '3iJnwqAn8tLjm+ep'
+
+# Establish connection using mysql.connector
+connection = mysql.connector.connect(
+    host=host,
+    database=database,
+    user=user,
+    password=password
+)
+
+db_Info = connection.get_server_info()
+print("Connected to MySQL Server version", db_Info)
+
+# Create an engine using mysql connector 
+engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{database}')
 
 @app.route('/')
 def homepage():
@@ -13,6 +35,16 @@ def homepage():
 def html_page(page_name):
     return render_template(page_name)
 
+@app.route('/personal')
+def personal():
+    cursor = connection.cursor()
+    cursor.execute("SELECT hobby FROM interests")
+    result = cursor.fetchall()
+    interests = []
+    for row in result:
+        interests.append(row[0]) 
+    cursor.close()
+    return render_template('personal_interest.html', interests=interests)
 
 # def write_to_file(data):
 #     with open('database.txt', mode='a') as database:
@@ -40,5 +72,5 @@ def submit_form():
     else:
         return 'Something went wrong!'
 
-if __name__=='__main__':
-   homepage()
+if __name__ == '__main__':
+    app.run(debug=True)
